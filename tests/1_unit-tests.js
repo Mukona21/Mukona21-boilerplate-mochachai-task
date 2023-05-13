eab146063af69de6d4029419eca27b12b20c1f3a
Mutshinye <Mutshinyen@gmail.com>
2023-05-13 02:39:58 +0200

    initial commit

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -65,7 +65,7 @@ test('#isOk, #isNotOk', function() {
       assert.isAtMost('hello'.length, 5);
       assert.isAbove(1, 0);
       assert.isAbove(Math.PI, 3);
-      assert.isAtmost(1 - Math.random(), 1);
+      assert.isAtMost(1 - Math.random(), 1);
     });
     // #9
     test('#isBelow, #isAtLeast', function () {

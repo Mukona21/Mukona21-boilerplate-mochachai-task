64f67c89173fa31a769a1bd7fcf2821283299e25
Mutshinye <Mutshinyen@gmail.com>
2023-05-13 03:21:48 +0200

    initial commit

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -152,8 +152,8 @@ test('#isOk, #isNotOk', function() {
     test('#typeOf, #notTypeOf', function () {
       assert.typeOf(myCar, 'object');
       assert.typeOf(myCar.model, 'string');
-      assert.typeOf(airlinePlane.wings, 'string');
-      assert.notTypeOf(airlinePlane.engines, 'array');
+      assert.notTypeOf(airlinePlane.wings, 'string');
+      assert.typeOf(airlinePlane.engines, 'array');
       assert.typeOf(myCar.wheels, 'number');
     });
     // #18

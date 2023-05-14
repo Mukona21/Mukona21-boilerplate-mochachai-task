42844bfb366784159d170732a101a0c9e5f9b338
Mutshinye <Mutshinyen@gmail.com>
2023-05-14 13:09:35 +0200

    initial commit

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -83,64 +83,46 @@ suite('Functional Tests', function () {
     });
 });
 const Browser = require('zombie');
-Browser.site = 'https://boilerplate-mochachai.luizfernandorg.repl.co';
-const browser = new Browser();
-suite('Functional Tests with Zombie.js', function() {
+Browser.localhost('example.com', (process.env.PORT || 3000));
+
+suite('Functional Tests with Zombie.js', function () {
   this.timeout(5000);
-  
+  const browser = new Browser();
   suiteSetup(function(done) {
-    return browser.visit('/', done);
-  });
- /*
-  suite('Headless browser', function() {
+  return browser.visit('/', done());
+});
+
+  suite('Headless browser', function () {
     test('should have a working "site" property', function() {
       assert.isNotNull(browser.site);
     });
   });
-   
+
   suite('"Famous Italian Explorers" form', function () {
     // #5
-    test('submit "surname" : "Colombo" - write your e2e test...', function(done) {
-  // fill the form...
-  // then submit it pressing 'submit' button.
-  //
-  // in the callback...
-  // assert that status is OK 200
-  // assert that the text inside the element 'span#name' is 'Cristoforo'
-  // assert that the text inside the element 'span#surname' is 'Colombo'
-  // assert that the element(s) 'span#dates' exist and their count is 1
-  browser.fill('surname', 'Colombo').pressButton('submit', function() {
-    /** YOUR TESTS HERE, Don't forget to remove assert.fail() **/
+    test('Submit the surname "Colombo" in the HTML form', function (done) {
+      browser.fill('surname','Colombo').then(() => {
+      browser.pressButton('submit',() => {
+        browser.assert.success();
+        browser.assert.text('span#name','Cristoforo');
+        browser.assert.text('span#surname','Colombo');
+        browser.assert.elements('span#dates',1);
+      done();
+      });
+      });
 
-    // pressButton is Async.  Waits for the ajax call to complete...
-
-    // assert that status is OK 200
-    browser.assert.success();
-    // assert that the text inside the element 'span#name' is 'Cristoforo'
-    browser.assert.text('span#name', 'Cristoforo');
-    // assert that the text inside the element 'span#surname' is 'Colombo'
-    browser.assert.text('span#surname', 'Colombo');
-    // assert that the element(s) 'span#dates' exist and their count is 1
-    browser.assert.element('span#dates', 1);
-
-    done(); // It's an async test, so we have to call 'done()''
-  });
-
-
+    });
     // #6
-    test('submit "surname" : "Vespucci" - write your e2e test...', function(done) {
-  // fill the form, and submit.
-  browser.fill('surname', 'Vespucci').pressButton('submit', function() {
-    // assert that status is OK 200
-    browser.assert.success();
-    // assert that the text inside the element 'span#name' is 'Amerigo'
-    browser.assert.text('span#name', 'Amerigo');
-    // assert that the text inside the element 'span#surname' is 'Vespucci'
-    browser.assert.text('span#surname', 'Vespucci');
-    // assert that the element(s) 'span#dates' exist and their count is 1
-    browser.assert.element('span#dates', 1);
-
-    done();
+    test('Submit the surname "Vespucci" in the HTML form', function (done) {
+      browser.fill('surname','Vespucci').then(() => {
+      browser.pressButton('submit',() => {
+        browser.assert.success();
+        browser.assert.text('span#name','Amerigo');
+        browser.assert.text('span#surname','Vespucci');
+        browser.assert.elements('span#dates',1);
+      done();
+      });
+      });
+    });
   });
 });
-
